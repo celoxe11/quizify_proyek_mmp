379ae8161cbe6a9da33af9332df86a014c1aa589
Marcella Thamrin <marcella.t23@mhs.istts.ac.id>
2025-12-02 12:49:41 +0700

    buat home page, quiz page, login register desktop

---
+++ lib/presentation/widgets/question_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestionCard extends StatefulWidget {
+  const QuestionCard({super.key});
+
+  @override
+  State<QuestionCard> createState() => _QuestionCardState();
+}
+
+class _QuestionCardState extends State<QuestionCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
