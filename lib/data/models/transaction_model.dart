deb09f527358173de1a50fe9d690c57e2a0f7bea
Calysta <ophelia.c23@mhs.istts.ac.id>
2026-01-13 17:26:27 +0700

    update models

--- lib/data/models/transaction_model.dart
+++ lib/data/models/transaction_model.dart
@@ -1,17 +1,18 @@
 class TransactionModel {
   final String id;
-  final String? userId; 
-  final String item;
+  final String? userId;
+  final String item; // Nama barang (Entah itu Paket atau Item)
+  final String category; // [BARU] 'subscription' atau 'item'
   final double amount;
   final String status;
   final String method;
   final DateTime date;
 
-
   TransactionModel({
     required this.id,
     this.userId,
     required this.item,
+    required this.category,
     required this.amount,
     required this.status,
     required this.method,
@@ -19,17 +20,20 @@ class TransactionModel {
   });
 
   factory TransactionModel.fromJson(Map<String, dynamic> json) {
-    final amountString = json['amount']?.toString() ?? '0.0';
-    final amountValue = double.tryParse(amountString) ?? 0.0;
-
     return TransactionModel(
-      id: json['id'] ?? '',
-      userId: json['userId'] ?? null,
-      item: json['item'] ?? 'Unknown',
-      amount: amountValue, 
+      id: json['id']?.toString() ?? '',
+      userId: json['user_id']?.toString(),
+      
+      // [LOGIC BARU] Menentukan nama barang berdasarkan kategori
+      item: json['item_name'] ?? 'Unknown Item', 
+      
+      // [BARU] Kategori
+      category: json['category']?.toString() ?? 'subscription',
+      
+      amount: double.tryParse(json['amount']?.toString() ?? '0') ?? 0.0,
       status: json['status'] ?? 'pending',
-      method: json['method'] ?? '-',
-      date: DateTime.tryParse(json['date'] ?? '') ?? DateTime.now(),
+      method: json['payment_method'] ?? '-',
+      date: DateTime.tryParse(json['created_at'] ?? '') ?? DateTime.now(),
     );
   }
 }
